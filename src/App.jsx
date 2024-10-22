98d52512385541f98504ef9f17cdc5aae5e867e8
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-10-22 17:02:15 +0530

    added the react-redux package and wrapped the app inside the provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import CakeView from './feature/CakeView'
+import IceCreamView from './feature/IceCreamView'
+import Users from './feature/Users'
+
+
 
 function App() {
 
   return (
     <>
-      
+    <CakeView/>
+    <IceCreamView/>
+    <Users/>
     </>
   )
 }
---
+++ src/feature/Users.jsx
@@ -0,0 +1,10 @@
+const UsersView = () => {
+    return (
+        <div>
+            <h1>Users List </h1>
+            
+        </div>
+    )
+}
+
+export default UsersView
